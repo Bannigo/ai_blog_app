97e0a944a3a0b0350b1b360a7037a54f02e3333e
Bannigo <yashshah5070@gmail.com>
2024-11-17 12:29:13 +0100

    folder restructured

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ai_blog_app_backend project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
